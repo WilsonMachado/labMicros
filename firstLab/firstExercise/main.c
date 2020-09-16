18a1c605f8e987aeadafe67f07484384d990b670
Wilson <wilson.machado@correounivalle.edu.co>
2020-09-16 01:32:20 -0500

    En esta fase, se agregaron las instrucciones necesarias para que, mediante el teclado, se pudiera introducir la siguiente información: número N de parejas de datos y datos en formato (x,y). Se almacena la información y las variables en formato double.

--- firstLab/firstExercise/main.c
+++ firstLab/firstExercise/main.c
@@ -2,26 +2,44 @@
 #include <stdlib.h>
 #include <math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define cols 2 // Como son parejas, la matriz donde se almacenarán tendrán 2 columnas y n filas (n parejas).
 
 int main(int argc, char *argv[]) {
 	
-	char num_in[8]; 		// Variable para recibir datos de consola
-	double num_out[2]; 		// Variable para almacenar los números 
-	int i;					// Variable de control ciclo For
-	
-	for(i = 0; i < 2; i = i+1){
-	
-		scanf("%s", &num_in); 	  // Escaneo de entrada de teclado
-		num_out[i] = atof(num_in); // Conversión string a double
-					
-	}
-	
-	
-	printf("Potencia: %4.2f\n", pow(num_out[0],2)); // Cálculo de potencia al cuadrado
-	printf("Raiz: %4.2f\n", sqrt(num_out[1]));		// Cálculo de raíz cuadrada	
+	int i,n;	
+	double **pairs; // Matrix de n x 2 para almacenar las parejas
 
-    system("pause");
+	printf("Introduzca el número de parejas que desea ingresar: ");
+	scanf("%d", &n);
+
+	pairs = (double **)malloc(n*sizeof(double)); // Le añado la cantidad de filas a la matriz
+		
+	for(i = 0; i < n; i = i+1){ // Le añado la cantidad de columnas la matriz
+
+		pairs[i] = 	(double *)malloc(cols*sizeof(double)); 					
+	}
+
+	printf("Introduzca las parejas, con cada valor separado por espacio (e.j: x y): \n");
+
+	for(i = 0; i < n; i = i+1){				// Introduzco los valores de la matriz
+
+		scanf("%lf", &pairs[i][0]);		
+		scanf("%lf", &pairs[i][1]); 
+
+		printf("\n");		
+	}	
+
+	printf("La matriz introducida es: \n"); 
+	
+	for(i = 0; i < n; i = i+1){				// Imprimo la matriz de las parejas para saber cómo quedaron
+		
+		printf("%.2f ", pairs[i][0]);
+		printf("%.2f \n", pairs[i][1]); 					
+	}
+
+	printf("\n");
+
+	system("pause");    
 	
 	return 0;
 }
